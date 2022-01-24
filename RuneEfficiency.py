c171fa8df4d24867c9a00f7309af9ed7f5f14160
G Huang <ghuang582@gmail.com>
2022-01-25 00:05:46 +1100

    Added code to calculate max efficiency. Adjusted formula for total efficiency by halving the efficiency values of non-SPD flat stats.

--- RuneEfficiency.py
+++ RuneEfficiency.py
@@ -4,7 +4,10 @@ import re
 
 max_stats = pd.read_csv(r"C:\Users\Admin\Desktop\SWOverlay\max_stats_table.csv")
 
-rune = [('DEF ', '+160 '), ('Accuracy ', '+7%'), ('CRI Dmg ', '+6%'), ('SPD ', '+24 '), ('HP ', '+12%')]
+# rune = [('DEF ', '+160 '), ('Accuracy ', '+7%'), ('CRI Dmg ', '+6%'), ('SPD ', '+24 '), ('HP ', '+12%')]
+# rune = [('DEF ', '+160 '), ('ATK ', '+61'), ('CRI Rate ', '+17%'), ('CRI Dmg ', '6% '), ('HP ', '+363')]
+rune = [('DEF ', '+160 '), ('ATK ', '+19'), ('Accuracy ', '+4%'), ('CRI Rate ', '4% ')]
+rune_level = 0
 
 # Trim white space from each entry
 rune = [[desc.strip() for desc in line] for line in rune]
@@ -15,11 +18,20 @@ rune_df = pd.DataFrame(rune, columns = ["stat", "value"])
 rune_df['raw_value'] = rune_df['value'].str.replace(r"\D+", "", regex = True).astype('int')
 rune_df['type'] = np.where(rune_df['value'].str.find("%") > 0, "Percent", "Flat")
 
-# Join max_stats data onto rune dataframe
+# Join max_stats data on to rune dataframe
 rune_merged = pd.merge(rune_df, max_stats, on = ["stat", "type"], how = "left")
 
 # Calculate efficiency at a per stat level
 rune_merged['efficiency'] = rune_merged["raw_value"] / rune_merged["max"]
 
+# Halve the efficiency values of flat stats that aren't speed (SPD)
+rune_merged['efficiency'] = np.where((rune_merged['type'] == "Flat") & (rune_merged['stat'] != "SPD"), 0.5 * rune_merged['efficiency'], rune_merged['efficiency'])
+
 # Calculate total efficiency of rune
-print((1 + rune_merged['efficiency'][1:].sum()) / 2.8)
+total_eff = (1 + rune_merged['efficiency'][1:].sum()) / 2.8
+print(total_eff)
+# print(rune_merged)
+
+# Calculate max efficiency of rune
+max_eff = total_eff + max((12 - rune_level)/3, 0) * 0.2 / 2.8
+print(max_eff)
