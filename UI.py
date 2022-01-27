79095324f97abc0a27d7d3272510a91147880082
G Huang <ghuang582@gmail.com>
2022-01-28 01:02:03 +1100

    Added code to update UI when it receives update call

--- UI.py
+++ UI.py
@@ -1,6 +1,12 @@
 import sys
 from PyQt5.QtWidgets import QMainWindow, QApplication
 from PyQt5 import uic
+import cv2
+
+import RecordScreen
+import BoxDetection
+import OCR
+import RuneEfficiency
 
 Ui_MainWindow, QtBaseClass = uic.loadUiType("SWtemplate.ui")
 
@@ -10,11 +16,68 @@ class MyApp(QMainWindow):
         self.ui = Ui_MainWindow()
         self.ui.setupUi(self)
 
-        self.inputRune()
+        # self.updateRune()
+        self.ui.CallUpdate.clicked.connect(self.callUpdate)
 
-    def inputRune(self, rune=[]):
-        if rune == []:
+    def updateRune(self, rune = [], efficiency = []):
+        if rune[1] == []:
+            print('err1')
+            print(rune)
             self.ui.MainStat.setText("hi")
+        else:
+            print('state2')
+            print(rune)
+            MainStat = ''.join(map(str, rune[1][0]))
+            self.ui.MainStat.setText(MainStat)
+
+            hasInnate = rune[0][2]
+
+            if hasInnate == 1:
+                Innate = ''.join(map(str, rune[1][1]))
+                self.ui.Innate.setText(Innate)
+            print('err2')
+            Slot1 = ''.join(map(str, rune[1][1 + hasInnate]))
+            self.ui.Slot1.setText(Slot1)
+
+            Slot2 = ''.join(map(str, rune[1][2 + hasInnate]))
+            self.ui.Slot2.setText(Slot2)
+
+            Slot3 = ''.join(map(str, rune[1][3 + hasInnate]))
+            self.ui.Slot3.setText(Slot3)
+
+            Slot4 = ''.join(map(str, rune[1][4 + hasInnate]))
+            self.ui.Slot4.setText(Slot4)          
+            print('err3')
+            self.ui.Rarity.setText(rune[0][1])
+            self.ui.CurrEff.setText("{0:.2%}".format(efficiency[0]))
+            self.ui.MaxEff.setText("{0:.2%}".format(efficiency[1]))      
+    
+    def callUpdate(self):
+        print("update")
+
+        snapshot = RecordScreen.screenGrab()
+        cropped = BoxDetection.crop_boxes(snapshot)
+        print('mid')
+        rune = OCR.do_ocr(cropped[0])
+        eff = RuneEfficiency.CalcEff(rune)
+
+        print('call')
+        
+        self.updateRune(rune, eff)
+
+        # n = 20
+        # for img in cropped:
+        #     cv2.imshow('{n}'.format(n = n), img)
+        #     n += 1
+
+        #     if n > 5:
+        #         break
+        
+        # cv2.waitKey(0)
+
+
+cropped_dir_path = "C:/Users/Admin/Desktop/SWOverlay/Cropped/"
+
         
 app = QApplication(sys.argv)
 window = MyApp()
