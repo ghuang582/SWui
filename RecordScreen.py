531d3f073532d2289f67aa3b701bd9d7244da4c0
G Huang <ghuang582@gmail.com>
2022-01-28 23:39:15 +1100

    Added function to take screenshot of NoxPlayer directly. Requires client to be on main screen if user has multiple monitors

--- RecordScreen.py
+++ RecordScreen.py
@@ -1,19 +1,28 @@
 import sys
-import PIL
+import win32gui
 from PIL import Image, ImageGrab
-import cv2
 
 use_grab = True
 
 def screenGrab( rect =[2560, 25, 885, 540] ):
-    """ Given a rectangle, return a PIL Image of that part of the screen.
-        Handles a Linux installation with and older Pillow by falling-back
-        to using XLib """
-    global use_grab
     x, y, width, height = rect
-
-    if ( use_grab ):
-        image = PIL.ImageGrab.grab( bbox=[ x, y, x + width, y + height ], all_screens = True )
+    image = ImageGrab.grab( bbox=[ x, y, x + width, y + height ], all_screens = True )
 
     return image
+
+def screenshot(window_title=None):
+    if window_title:
+        hwnd = win32gui.FindWindow(None, window_title)
+        if hwnd:
+            win32gui.SetForegroundWindow(hwnd)
+            x, y, x1, y1 = win32gui.GetClientRect(hwnd)
+            x, y = win32gui.ClientToScreen(hwnd, (x, y))
+            x1, y1 = win32gui.ClientToScreen(hwnd, (x1 - x, y1 - y))
+
+            im = ImageGrab.grab(bbox = [x, y, x+x1, y+y1], all_screens=True)
+            
+            return im
+        else:
+            print('Window not found!')
+
 
